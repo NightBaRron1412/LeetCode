1cb06a9e38b5e2a1df5a94fdd089f19052f7b417
Amir Shetaia <amoor22009@gmail.com>
2024-08-22 12:32:52 +0300

    Time: 0 ms (100%), Space: 7.7 MB (53.96%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.c
@@ -0,0 +1,14 @@
+int findComplement(int num) {
+    int num1 = num;
+    unsigned op = 0b1;
+    while (num1 != 1){
+        num1 = num1 >> 1;
+        op = op << 1;
+        op = op + 0b1;
+    }
+    printf("%b     %b", num, op);
+    return ~num & op;
+}
+
+
+
