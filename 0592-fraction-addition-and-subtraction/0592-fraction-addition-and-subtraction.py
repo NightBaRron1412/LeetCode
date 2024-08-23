c71a4cc2ea8fe7e54794b39663871cd01cba77fb
Amir Shetaia <amoor22009@gmail.com>
2024-08-23 17:56:55 +0300

    Time: 38 ms (43.02%), Space: 16.7 MB (6.2%) - LeetHub

---
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.py
@@ -0,0 +1,55 @@
+class Solution:
+
+    def fractionAddition(self, expression: str) -> str:
+        firstposflag = False
+        numlist = []
+        denlist = []
+        numerator = 0
+
+        if expression[0] != "-":
+            firstposflag = True
+
+        for i in range(len(expression)):
+            if expression[i] == "/":
+
+                if not numlist and firstposflag:
+                    if(expression[0] == "1" and expression[i - 1] == "0"):
+                        numlist.append(10)
+                    else:
+                        numlist.append(int(expression[i - 1]))
+                else:
+                    if (expression[i - 3] == "-"):
+                        numlist.append(-10)
+                    elif (expression[i - 2] == "-"):
+                         numlist.append(-1 * int(expression[i - 1]))
+                    elif (expression[i - 2] == "1"):
+                        numlist.append(10)
+                    else:
+                        numlist.append(int(expression[i - 1]))
+
+                if (expression[i + 1] == "1" and i == len(expression) - 3):
+                    denlist.append(10)
+                elif (expression[i + 1] == "1" and i == len(expression) - 2):
+                    denlist.append(int(expression[i + 1]))
+                elif (expression[i + 1] == "1" and expression[i + 2] == "0"):
+                    denlist.append(10)
+                else:
+                    denlist.append(int(expression[i + 1]))
+
+        denlcm = reduce(lcm, denlist)
+
+        for i in range(len(numlist)):
+            numerator += (numlist[i] * (denlcm / denlist[i]))
+            numerator = int(numerator)
+        cd = gcd(numerator, denlcm)
+
+        print(numlist, denlist, denlcm, numerator)
+
+        return f"{numerator // cd}/{denlcm // cd}"
+
+        
+
+
+
+
+
