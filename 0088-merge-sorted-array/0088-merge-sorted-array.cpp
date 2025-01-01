889cc09205b2f07c03259c79bc874bc53ff73bf2
Amir Shetaia <amoor22009@gmail.com>
2025-01-01 17:49:53 -0500

    Time: 0 ms (100%), Space: 12.4 MB (5.5%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i {0};
-        int k {m - 1};
-        for (size_t j = 0; j < m + n; j++){
-            if (m == 0){
-                nums1 = nums2;
-                break;
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while(i >= 0 && j >= 0){
+            if (nums1.at(i) > nums2.at(j)){
+                nums1.at(k--) = nums1.at(i--);
             }
-            if (i < n && nums1.at(j) > nums2.at(i)){
-                nums1.insert(nums1.begin() + j, nums2.at(i++));
-                nums1.pop_back();
-                k++;
+            else{
+                nums1.at(k--) = nums2.at(j--);
             }
-            else if (i < n && j > k){
-                cout << "Entering second case";
-                nums1.insert(nums1.begin() + j, nums2.at(i++));
-                nums1.pop_back();
-            }
+        }
+
+        while (j >=0 ){
+            nums1.at(k--) = nums2.at(j--);
         }
     }
 };
