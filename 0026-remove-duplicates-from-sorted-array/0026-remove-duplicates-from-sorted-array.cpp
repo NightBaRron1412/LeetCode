5d4eb7d070dfc334f6624c8443515d2669da2285
Amir Shetaia <amoor22009@gmail.com>
2025-01-01 19:03:42 -0500

    Time: 0 ms (100%), Space: 22.5 MB (49.98%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        unordered_map<int, int> hist;
-        int index {0};
-        for (int i = 0; i < nums.size(); i++){
-            if(hist.find(nums[i]) == hist.end()){
-                nums[index++] = nums[i];
-                hist[nums[i]] = 1;
-
+        int write_index {1};
+        for (int i = 1; i < nums.size(); i++){
+            if (nums[write_index - 1] != nums[i]){
+                nums[write_index++] = nums[i] ;
             }
         }
-        return index;
+        return write_index;
     }
 };
