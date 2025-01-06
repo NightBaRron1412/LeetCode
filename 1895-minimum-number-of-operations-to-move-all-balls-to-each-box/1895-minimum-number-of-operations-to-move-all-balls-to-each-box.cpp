2cb49ed6e8298a1732b12ff6a0580f9b9442a79b
Amir Shetaia <amoor22009@gmail.com>
2025-01-05 22:02:50 -0500

    Time: 66 ms (40.86%), Space: 12.3 MB (40.56%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
-        vector<int> non_empty_boxes (0, boxes.size());
-        vector<int> result (0, boxes.size());
+        vector<int> result(boxes.size(), 0);
         for (size_t i = 0; i < boxes.size(); i++){
             if (boxes[i] == '1'){
-                non_empty_boxes.push_back(i);
+                for (size_t j = 0; j < boxes.size(); j++){
+                    result[j] += abs(static_cast<int>(j - i));
+                }
             }
         }
-        for (size_t i = 0; i < boxes.size(); i++){
-            int ans {0};
-            for (size_t j = 0; j < non_empty_boxes.size(); j++){
-                ans += abs(static_cast<int>(i - non_empty_boxes[j]));
-            }
-            result.push_back(ans);
-        }
+
         return result;
     }
 };
