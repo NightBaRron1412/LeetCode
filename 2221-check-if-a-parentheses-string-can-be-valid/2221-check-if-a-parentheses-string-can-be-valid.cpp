75b95293b4b2d531a66d76939ed9e0ec0ce3a365
Amir Shetaia <amoor22009@gmail.com>
2025-01-11 22:25:50 -0500

    Time: 7 ms (95.69%), Space: 30.1 MB (53.05%) - LeetHub

---
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool canBeValid(string s, string locked) {
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+
+        int minOpen {0};
+        int maxOpen {0};
+        for (int i = 0; i < s.length(); ++i) {
+            if (locked[i] == '1') {
+                if (s[i] == '(') {
+                    minOpen++;
+                    maxOpen++;
+                } else {
+                    minOpen--;
+                    maxOpen--;
+                }
+            } else {
+                minOpen--;
+                maxOpen++;
+            }
+
+            if (maxOpen < 0) {
+                return false;
+            }
+            minOpen = max(minOpen, 0);
+        }
+
+        int minClose = 0, maxClose = 0;
+        for (int i = s.length() - 1; i >= 0; --i) {
+            if (locked[i] == '1') {
+                if (s[i] == ')') {
+                    minClose++;
+                    maxClose++;
+                } else {
+                    minClose--;
+                    maxClose--;
+                }
+            } else {
+                minClose--;
+                maxClose++;
+            }
+
+            if (maxClose < 0) {
+                return false;
+            }
+            minClose = max(minClose, 0);
+        }
+
+        return minOpen == 0 && minClose == 0;
+    }
+};
