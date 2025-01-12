fc115322fba7b3cd31520093c854686f170c8764
Amir Shetaia <amoor22009@gmail.com>
2025-01-11 22:27:21 -0500

    Time: 16 ms (44.16%), Space: 30 MB (79.7%) - LeetHub

--- 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
@@ -1,48 +1,67 @@
 class Solution {
 public:
     bool canBeValid(string s, string locked) {
-        if (s.length() % 2 != 0) {
+        if (s.length() % 2 != 0)
+        {
             return false;
         }
 
         int minOpen {0};
         int maxOpen {0};
-        for (int i = 0; i < s.length(); ++i) {
-            if (locked[i] == '1') {
-                if (s[i] == '(') {
+
+        for (int i = 0; i < s.length(); ++i)
+        {
+            if (locked[i] == '1')
+            {
+                if (s[i] == '(')
+                {
                     minOpen++;
                     maxOpen++;
-                } else {
+                }
+                else
+                {
                     minOpen--;
                     maxOpen--;
                 }
-            } else {
+            }
+            else
+            {
                 minOpen--;
                 maxOpen++;
             }
 
-            if (maxOpen < 0) {
+            if (maxOpen < 0)
+            {
                 return false;
             }
             minOpen = max(minOpen, 0);
         }
 
-        int minClose = 0, maxClose = 0;
-        for (int i = s.length() - 1; i >= 0; --i) {
-            if (locked[i] == '1') {
-                if (s[i] == ')') {
+        int minClose {0};
+        int maxClose {0};
+        for (int i = s.length() - 1; i >= 0; --i)
+        {
+            if (locked[i] == '1')
+            {
+                if (s[i] == ')')
+                {
                     minClose++;
                     maxClose++;
-                } else {
+                }
+                else
+                {
                     minClose--;
                     maxClose--;
                 }
-            } else {
+            }
+            else
+            {
                 minClose--;
                 maxClose++;
             }
 
-            if (maxClose < 0) {
+            if (maxClose < 0)
+            {
                 return false;
             }
             minClose = max(minClose, 0);
