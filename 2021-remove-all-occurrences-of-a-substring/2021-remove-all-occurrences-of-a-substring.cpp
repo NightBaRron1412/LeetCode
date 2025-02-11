0128ff1aa514261c1952f522de45f10e386b1550
Amir Shetaia <amoor22009@gmail.com>
2025-02-10 22:21:52 -0500

    Time: 0 ms (100%), Space: 9.2 MB (99.58%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,19 +1,10 @@
 class Solution {
 public:
-    string removeOccurrences(string s, string part) {
-        int pos{0};
+    string removeOccurrences(string s, const string& part) {
+        size_t pos;
         size_t part_len = part.size();
-        while (true)
-        {
-            pos = s.find(part);
-            if (pos == string::npos)
-            {
-                break;
-            }
-            else
-            {
-                s.erase(pos, part_len);
-            }
+        while ((pos = s.find(part)) != string::npos) {
+            s.erase(pos, part_len);
         }
         return s;
     }
