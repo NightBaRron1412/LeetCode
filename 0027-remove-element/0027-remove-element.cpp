a35bfd6e6ccad0a1b4c1ec70431c7ba7c235e77b
Amir Shetaia <amoor22009@gmail.com>
2025-08-06 21:34:06 -0400

    Time: 0 ms (100%), Space: 11.8 MB (6.2%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int count {0};
-        for (int i = 0; i < nums.size(); i++){
-            if (nums[i] != val){
-                nums[count++] = nums[i];
+        int p1 = 0; // a pointer that stops when we see the value we want to remove so we can replace it with the value from the other pointer
+        size_t n = nums.size();
+
+        for (int p2 = 0; p2 < n; p2++) //p2 is a pointer that iterates through nums
+        {
+            if (nums[p2] != val)
+            {
+                nums[p1++] = nums[p2]; // assign value from p2 to p1 and advance p1
             }
         }
-        return count;
+        return p1; //since we only advance p1 when we see a value other than the value we want to remove it should has the count of other numbers
     }
 };
