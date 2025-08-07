db144105d6e2457a67c265b21ae29c5183e81558
Amir Shetaia <amoor22009@gmail.com>
2025-08-06 21:56:38 -0400

    Time: 0 ms (100%), Space: 22.7 MB (50.65%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int p1 = 0; // a pointer that will replace items when the other pointer finds a new unique item
-        int n = nums.size();
+        int n = nums.size(); // number of elements inside nums
 
-        for (int p2 = 0; p2 < n; p2++)
+        for (int p2 = 0; p2 < n; p2++) // p2 is an iterator that iterates throught the array to look for unique elements
         {
             if (nums[p2] !=  nums[p1])
             {
-                nums[++p1] = nums[p2];
+                nums[++p1] = nums[p2]; // if the element is unique we replace the item at p1 with the item at p2 and we advance p1 to next loc
             }
         }
 
-        return p1 + 1;
+        return p1 + 1; // we move p when we find a new unique element so it should contain the number of unique elements + the first element in the array
     }
 };
