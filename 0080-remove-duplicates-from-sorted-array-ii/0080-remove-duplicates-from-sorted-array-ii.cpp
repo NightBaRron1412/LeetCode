63e3a531a59cf8bf294b5b83e6393c9974897706
Amir Shetaia <amoor22009@gmail.com>
2025-08-06 23:17:05 -0400

    Time: 11 ms (34.95%), Space: 19.4 MB (79.42%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int write_index {1};
-        int count{1};
+        // Pointer p1 indicates the position to place the next allowed element.
+        // We start from index 2 since up to two duplicates are allowed.
+        int p1 = 2;
+        int n = nums.size(); // Total number of elements in the array.
 
-        for (int i = 1; i < nums.size(); i++){
-            if (nums[i] != nums[i - 1]){
-                count = 0;
-            }
-            if(nums[write_index - 1] != nums[i] || count < 2){
-                nums[write_index++] = nums[i];
-                count++;
+         if (n <= 2)
+         {
+            return n; // If size is 2 or less, all elements are valid
+         }
+
+        // Iterate through the array starting from index 0
+        for (int p2 = 2; p2 < n; p2++) {
+            // Check if current element is different from the element two positions before p1.
+            // This ensures no more than two duplicates are allowed.
+            if (nums[p2] != nums[p1 - 2]) {
+                // Place the valid element at position p1 and increment p1
+                nums[p1++] = nums[p2];
             }
         }
-        return write_index;
+
+        // p1 is the new length of the array after limiting duplicates to at most two
+        return p1;
     }
 };
