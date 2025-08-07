8eda787a5a158bf309bcd89b8680709fb08fbfbb
Amir Shetaia <amoor22009@gmail.com>
2025-08-06 23:10:50 -0400

    Time: 0 ms (100%), Space: 22.7 MB (19.81%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int p1 = 1; // a pointer that will replace items when the other pointer finds a new unique item
-        int n = nums.size(); // number of elements inside nums
+        // Pointer p1 marks the position to place the next unique element.
+        // Start from index 1 since the first element is always unique.
+        int p1 = 1;
+        int n = nums.size(); // Total number of elements in the array.
 
-        for (int p2 = 0; p2 < n; p2++) // p2 is an iterator that iterates throught the array to look for unique elements
-        {
-            if (nums[p2] !=  nums[p1 - 1])
-            {
-                nums[p1++] = nums[p2]; // if the element is unique we replace the item at p1 with the item at p2 and we advance p1 to next loc
+        // Traverse the array with p2 to find unique elements.
+        for (int p2 = 0; p2 < n; p2++) {
+            // If the current element is different from the last added unique element
+            if (nums[p2] != nums[p1 - 1]) {
+                // Place the new unique element at p1 and move p1 forward
+                nums[p1++] = nums[p2];
             }
         }
 
-        return p1; // we move p when we find a new unique element so it should contain the number of unique elements + the first element in the array
+        // p1 represents the length of the array with duplicates removed
+        return p1;
     }
 };
