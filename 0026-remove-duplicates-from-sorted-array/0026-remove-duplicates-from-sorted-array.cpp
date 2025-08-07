cb81ff7fd8b745426c71a43f0e3eb99bafffb7d4
Amir Shetaia <amoor22009@gmail.com>
2025-08-06 23:26:48 -0400

    Time: 0 ms (100%), Space: 22.7 MB (19.81%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -7,7 +7,7 @@ public:
         int n = nums.size(); // Total number of elements in the array.
 
         // Traverse the array with p2 to find unique elements.
-        for (int p2 = 0; p2 < n; p2++) {
+        for (int p2 = 1; p2 < n; p2++) {
             // If the current element is different from the last added unique element
             if (nums[p2] != nums[p1 - 1]) {
                 // Place the new unique element at p1 and move p1 forward
