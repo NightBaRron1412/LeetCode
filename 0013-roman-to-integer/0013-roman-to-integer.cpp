f3c9ee52efb7dcff387832fe29c9f6381bca4438
Amir Shetaia <amoor22009@gmail.com>
2025-08-18 01:29:30 -0400

    Time: 15 ms (9.99%), Space: 13.4 MB (19.62%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int res = 0;
-        size_t n = s.size();
-         unordered_map<char, int> roman_to_int = {
+        int res = 0;                     // Result accumulator
+        size_t n = s.size();             // Length of the Roman numeral string
+
+        // Mapping of Roman numeral characters to their integer values
+        unordered_map<char, int> roman_to_int = {
             {'I', 1},
             {'V', 5},
             {'X', 10}, 
@@ -13,20 +15,23 @@ public:
             {'M', 1000}
         };
 
+        // Traverse each character except the last one
         for (int i = 0; i < n - 1; i++)
         {
+            // If current value < next value, it's a subtractive notation (e.g., IV = 4)
             if (roman_to_int[s[i]] < roman_to_int[s[i + 1]])
             {
-                res -= roman_to_int[s[i]];
+                res -= roman_to_int[s[i]];  // Subtract current value
             }
             else
             {
-                res += roman_to_int[s[i]];
+                res += roman_to_int[s[i]];  // Otherwise, add current value
             }
         }
 
+        // Always add the value of the last character
         res += roman_to_int[s[n - 1]];
 
-        return res;
+        return res;  // Final integer value
     }
 };
