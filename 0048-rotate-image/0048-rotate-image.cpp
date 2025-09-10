460368fe616583097cbecb0df7b67a37e844aa72
Amir Shetaia <amoor22009@gmail.com>
2025-09-09 23:39:12 -0400

    Time: 0 ms (100%), Space: 10.3 MB (35.4%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        size_t n = matrix.size();
+        size_t n = matrix.size(); // Number of rows/columns (matrix is n x n)
 
+        // Step 1: Reverse the order of rows (vertical flip)
         reverse(matrix.begin(), matrix.end());
 
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = i + 1; j < n; j++)
-            {
+        // Step 2: Transpose the matrix (swap across the main diagonal)
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
+        // After these two steps, the matrix is rotated 90 degrees clockwise
     }
 };
